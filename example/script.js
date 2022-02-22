c3d31fa14ae5b6a92ca02d27ed270a2325713e48
werlang <pswerlang@gmail.com>
2022-02-22 09:09:49 -0300

    * Initial commit.

---
+++ example/script.js
@@ -0,0 +1,19 @@
+const obj = {
+    foo: 'bar',
+    num: 1,
+    array: [
+        { name: 'obj1', value: 1 },
+        { name: 'obj2', value: 10 },
+        { name: 'obj3', value: 100 },
+    ]
+};
+
+(async () => {
+    await import('../json2html.js');
+    
+    const code = JSON.toHTML(obj);
+    loadJSONTheme(['default_dark', 'default_light']);
+
+    document.querySelectorAll('.json-container')[0].innerHTML = code;
+    document.querySelectorAll('.json-container')[1].innerHTML = code;
+})();
---
+++ json2html.js
@@ -0,0 +1,62 @@
+// pretty print json inside html
+JSON.toHTML = (json, roll) => {
+    if (!roll) {
+        let res = `<div class="json indent">${JSON.toHTML(json, true)}</div>`;
+        return Array.isArray(json) ? `[${res}]` : `{${res}}`;
+    }
+    if (Array.isArray(json)) {
+        return json.map((e, i) => {
+            const comma = i < json.length - 1 ? ',' : '';
+            let value = JSON.toHTML(e, true);
+            if (typeof e === 'object') {
+                value = `{${value}}`;
+            }
+            return `<div class="json indent">${value}${comma}</div>`;
+        }).join('');
+    }
+    else if (json == null) {
+        return `<span class="json">null</span>`;
+    }
+    else if (typeof json === 'object') {
+        return Object.entries(json).map(([key, value]) => {
+            let valueStr = JSON.toHTML(value, true);
+            if (Array.isArray(value)) {
+                valueStr = `[${valueStr}]`;
+            }
+            else if (value == null) {
+                valueStr = `null`;
+            }
+            else if (typeof value === 'object') {
+                valueStr = `{${valueStr}}`;
+            }
+
+            const comma = Object.keys(json).slice(-1)[0] != key ? ',' : '';
+            return `<div class="json indent"><span class="json key">"${key}"</span>: ${valueStr}${comma}</div>`;
+        }).join('');
+    }
+    else {
+        const type = typeof json === 'string' ? 'string' : 'number';
+        if (type == 'string') {
+            json = `"${json}"`;
+        }
+        return `<span class="json ${type}">${json}</span>`;
+    }
+};
+
+window.loadJSONTheme = themes => {
+    if (!Array.isArray(themes)){
+        themes = [ themes ];
+    }
+
+    themes.forEach(async t => {
+        const theme = document.createElement('style');
+        theme.textContent = await (await fetch(`./themes/${t}.css`)).text();
+        document.head.insertAdjacentElement('beforeend', theme);
+    });
+};
+
+(async () => {
+    const style = document.createElement('style');
+    style.textContent = await (await fetch('./json2html.css')).text();
+    document.head.insertAdjacentElement('beforeend', style);
+})();
